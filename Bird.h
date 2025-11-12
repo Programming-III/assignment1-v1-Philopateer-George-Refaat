712ba789af820427b4111461409734d6ae22e4bb
Panda-9900 <philopateer.refaat@student.giu-uni.de>
2025-11-12 15:10:21 +0200

    Completed Course class

--- Bird.h
+++ Bird.h
@@ -1,14 +1,21 @@
-#ifndef BIRD_H
-#define BIRD_H
-
+#ifndef BIRD_H_INCLUDED
+#define BIRD_H_INCLUDED
 #include "Animal.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
-//define class bird here 
+class Bird : public Animal{
 
+    private:
+    float wingSpan;
 
+    public:
+    Bird();
+    Bird(float wingSpan);
+    ~Bird();
 
-
-
-
-
+    void setWingSpan(float ws);
+    float getWingSpan();
+};
 #endif
