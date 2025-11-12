c97d08b0c0b3dfb84e8842cbd60aa67a613ae752
Panda-9900 <philopateer.refaat@student.giu-uni.de>
2025-11-12 15:11:12 +0200

    Completed Course class

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,26 @@
-#ifndef MAMMAL_H
-#define MAMMAL_H
-
+#ifndef MAMMAL_H_INCLUDED
+#define MAMMAL_H_INCLUDED
 #include "Animal.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
-//define mammal class here
+class Mammal : public Animal{
+
+        private:
+        string furColor;
+
+        public:
+        Mammal();
+        Mammal(string furColor);
+        ~Mammal();
+
+        void setFurColor(string fc);
+        string getFurColor();
 
 
 
 
-
+};
 
 #endif
