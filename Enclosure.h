d34130d06e9b181f14dec2b610acc068aef906ad
Panda-9900 <philopateer.refaat@student.giu-uni.de>
2025-11-12 15:10:48 +0200

    Completed Course class

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,36 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
-
+#ifndef ENCLOSURE_H_INCLUDED
+#define ENCLOSURE_H_INCLUDED
 #include "Animal.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
-//define enclosure class here 
+class Enclosure{
+    private: 
+    Animal* animals;
+    int capacity;
+    int currentCount;
+
+    public:
+    Enclosure();
+    Enclosure(int capacity,int currentCount);
+    ~Enclosure();
+
+    
+    void addAnimals(Animal*a);
+    void displayAnimals();
 
 
+    void setCapacity(int x);
+    void setcurrentCount(int x);
+    
+    int getCapacity();
+    int getcurrentCount();
+  
+    
+ 
 
+   
 
-
-
-
-
-#endif
+};
+#endif 
